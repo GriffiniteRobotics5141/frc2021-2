cd11fe1d0bb4cd0af2156f6d9541384a62db2807
Winnetonka <priderobotics5141@gmail.com>
2021-03-04 15:20:18 -0600

    first commit

---
+++ Graveyard/1_TestRobot.java
@@ -0,0 +1,467 @@
+
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+//Robot 1-25, http://10.51.41.11:5801/ 
+
+/* to do list
+  organize imports
+*/
+package frc.robot;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.kauailabs.navx.frc.AHRS.SerialDataType;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.SerialPort;
+//import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  
+
+  private static final String kCenter = "Center";
+  private static final String kLeft = "Left";
+  private static final String kRight = "Right";
+  private static final String kOff = "Off";
+
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private String m_controlSelected;
+  private final SendableChooser<String> m_control = new SendableChooser<>();
+
+  private static final String kManual = "Manual";
+  private static final String kSensor = "Sensor";
+
+  private String m_challengeSelected;
+  private final SendableChooser<String> m_challange = new SendableChooser<>();
+
+  private static final String kComp = "Competition";
+  private static final String kTask1 = "Task1";
+
+  public double leftStick;
+  public double rightStick;
+
+  String Mode = "manual";// manual v. sensor
+
+  Joystick gamePad0 = new Joystick(0);
+  /*
+   * Button mapping 1 - AutoFace 2 - Color 3 - Rotate 4 - Conveyer input ? 5 -
+   * Intake toggle 6 - Shooter 7 - 8 - Auto Kill
+   */
+  VictorSP right0 = new VictorSP(0);
+  VictorSP right1 = new VictorSP(1);
+  VictorSP left0 = new VictorSP(2);
+  VictorSP left1 = new VictorSP(3);
+  SpeedControllerGroup leftDrive = new SpeedControllerGroup(left0, left1);
+  SpeedControllerGroup rightDrive = new SpeedControllerGroup(right0, right1);
+  DifferentialDrive driveTrain = new DifferentialDrive(leftDrive, rightDrive);
+
+  AHRS navx;
+  String navDrive = "null";
+  int setAngle = 0;
+  double angledYaw;
+  double AOC = 85; //Area of correction
+  Timer curveTimer;
+
+  Timer challengeTimer = new Timer();
+  int challengeTimerCheckpoint;
+  double route;
+  // NetworkTable table;
+  /** Limelight Modes
+   *  0 - use the LED Mode set in the current pipeline
+   *  1 - force off
+   *  2 - force blink
+   *  3 - force on
+   */
+  NetworkTableEntry tx;
+  NetworkTableEntry ty;
+  NetworkTableEntry ta;
+  NetworkTableEntry tv;
+  double x;
+  double y;
+  double a;
+  double v;
+
+  double minCorrectNavX = .34;
+  double maxCorrectNavX = .65;
+
+/*  enum ROUTES {
+    BLUE,
+    RED,
+    GREEN
+  }
+
+  if(ROUTES.BLUE) {}*/
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // m_choosers on bottom
+    SmartDashboard.putData("Auto positions", m_chooser);
+    m_chooser.addOption("Center", kCenter);
+    m_chooser.addOption("Left", kLeft);
+    m_chooser.addOption("Right", kRight);
+    m_chooser.setDefaultOption("Off", kOff);
+
+    SmartDashboard.putData("Control Mode", m_control);
+    m_control.setDefaultOption("Manual", kManual);
+    m_control.addOption("Sensor", kSensor);
+
+    SmartDashboard.putData("Game Mode", m_challange);
+    m_challange.setDefaultOption("Competition", kComp);
+    m_challange.addOption("Task1", kTask1);
+
+    // navx = new AHRS(SerialPort.Port.kMXP, SerialDataType.kProcessedData,
+    // (byte)50);
+    right0.setInverted(true); //false for flash
+    right1.setInverted(true); // false for flash
+    left0.setInverted(true); //true for flash
+    left1.setInverted(true); //true for flash
+
+    navx = new AHRS(SerialPort.Port.kMXP, SerialDataType.kProcessedData, (byte)50);
+    navx.zeroYaw();
+    navx.reset();
+    navDrive = "null";
+
+    curveTimer.stop();
+    curveTimer.reset();
+
+    challengeTimer.stop();
+    challengeTimer.reset();
+
+  //  CameraServer.getInstance().startAutomaticCapture(); //non-li\melight camera declaration????? why is it here.
+
+    // navx.reset();
+    // navx.zeroYaw();
+
+    table.getEntry("ledMode").setNumber(3);
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
+   *
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+
+    double yaw = navx.getYaw();
+    SmartDashboard.putNumber("Yaw",navx.getYaw());
+
+    final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    final NetworkTableEntry tx = table.getEntry("tx");
+    final NetworkTableEntry ty = table.getEntry("ty");
+    final NetworkTableEntry ta = table.getEntry("ta");
+    final NetworkTableEntry tv = table.getEntry("tv");
+
+    final double x = (tx.getDouble(0.0)); // x & y is negative because limelight is upsidedown
+    final double y = (ty.getDouble(0.0));
+    final double a = ta.getDouble(0.0);
+    final double v = tv.getDouble(0.0);
+
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightA", a);
+    
+    double ratioNavX;
+
+    if (Math.abs(yaw - setAngle) <= 180){
+      angledYaw = yaw - setAngle;
+    }else{
+      angledYaw = -Math.signum(yaw - setAngle)*(360-Math.abs(yaw - setAngle));
+    }
+
+    if(Math.abs(angledYaw) < (AOC)) {
+      ratioNavX =angledYaw/AOC;
+    } else if (angledYaw > 0){
+      ratioNavX = 1;
+    } else { ratioNavX = -1;}
+
+    //double sineWithSignum = Math.signum(ratioNavX)*(1-min)*Math.sin(ratioNavX*Math.PI/2)+(1+min)/2;
+    double sineNavX = Math.signum(ratioNavX)*((maxCorrectNavX - minCorrectNavX)/2)*Math.sin(Math.PI*(ratioNavX-.5))+Math.signum(ratioNavX)*((maxCorrectNavX + minCorrectNavX)/2);
+    
+  if(navDrive.length() > 0) {
+    switch (navDrive.charAt(0)) {
+      default :
+      minCorrectNavX = .39;
+      maxCorrectNavX = .65;
+      AOC = 85;
+      break;
+      case 'T' :
+        minCorrectNavX = .39; //.34
+        maxCorrectNavX = .65;
+        AOC = 85;
+
+        driveTrain.tankDrive(-sineNavX,sineNavX);
+        break;
+      case 'D' :
+        minCorrectNavX = 0.5;//.4
+        maxCorrectNavX = 1;//.75
+        AOC = 15;//15
+
+        double cCorrection = (-sineNavX > 0) ? -sineNavX : minCorrectNavX;
+        double ccCorrection  = (sineNavX > 0) ? sineNavX : minCorrectNavX;
+
+        driveTrain.tankDrive(cCorrection,ccCorrection); 
+    
+        break;
+  }
+  }
+
+}
+  @Override
+  public void autonomousInit() {
+
+    table.getEntry("ledMode").setNumber(3);
+
+    navx.zeroYaw();
+    navx.reset();
+
+    curveTimer.stop();
+    curveTimer.reset();
+
+    challengeTimer.stop();
+    challengeTimer.reset();
+    navDrive = "null";
+
+
+    
+
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    final NetworkTableEntry tx = table.getEntry("tx");
+    final NetworkTableEntry ty = table.getEntry("ty");
+    final NetworkTableEntry ta = table.getEntry("ta");
+    final NetworkTableEntry tv = table.getEntry("tv");
+
+    m_autoSelected = m_chooser.getSelected();
+    m_challengeSelected = m_challange.getSelected();
+
+
+    switch (m_autoSelected) {
+    case kLeft:
+      // Put left auto code here
+      driveTrain.tankDrive(-.5, .5);
+      break;
+    case kRight:
+      driveTrain.tankDrive(.5, -.5);
+      // Put right auto code here
+      break;
+    case kCenter:
+      break;
+    case kOff:
+      break;
+    default:
+      break;
+    }
+
+    SmartDashboard.putNumber("challengeTimer", challengeTimer.get());
+    SmartDashboard.putNumber("route number", challengeTimer.get());
+  
+      switch (m_challengeSelected) {
+        case kComp:
+          // Put left auto targetting and shooting code here
+          //driveTrain.tankDrive(-.5, .5);
+          break;
+
+        //Use Yellow Limelight Snapshot setting
+        case kTask1:
+          if (challengeTimer.get() == 0) {
+            route = y;
+            challengeTimer.start();
+          } 
+
+          if( route <= 0 ) { //blue config
+
+            if(challengeTimer.get() < 2) { // following seconds are ~1 second(s) are shorter
+              navDrive = "Drive";
+            } else if(((int)challengeTimer.get()) == 2){
+              turnThing(180, 2);
+            } 
+            else if (challengeTimer.get() < 5) {//this if is broken, also check if turn progress bollean is ok
+              navDrive = "Drive";
+            } else if(((int)challengeTimer.get()) == 5){
+              turnThing(0, 5);
+            } else {
+              challengeTimer.reset();
+            }
+
+
+            }
+
+          
+          
+           else  { //red config
+              //
+            }
+
+
+          break;
+        default:
+          break;
+    }
+
+
+
+  }
+
+  @Override
+  public void teleopInit() {
+
+    table.getEntry("ledMode").setNumber(3);
+
+    navx.zeroYaw();
+    navx.reset();
+
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+
+    m_controlSelected = m_control.getSelected(); 
+
+    SmartDashboard.putNumber("leftStick", gamePad0.getRawAxis(1));
+    SmartDashboard.putNumber("rightStick", gamePad0.getRawAxis(5));
+    
+    double leftStick = (-gamePad0.getRawAxis(1) * .6) * (gamePad0.getRawAxis(3) + 1);
+    // double leftStick =
+    // (-gamePad0.getRawAxis(1)*((gamePad0.getRawAxis(3)==1)?.6:.8));
+    double rightStick = (-gamePad0.getRawAxis(5) * .6) * (gamePad0.getRawAxis(3) + 1);
+    // double rightStick =
+    // (-gamePad0.getRawAxis(5)*((gamePad0.getRawAxis(3)==1)?.6:.8));
+    driveTrain.tankDrive(leftStick, rightStick);// 12/13 is motor ratio for simon none for flash
+
+    /*if(gamePad0.getRawButtonPressed(4)) {
+      recordInputs(leftStick, rightStick);
+    }*/
+    
+
+
+    if (gamePad0.getRawButton(1)){
+      navDrive = "Turn";
+    } else if (gamePad0.getRawButton(2)) {
+      navDrive = "Drive";
+    } else {
+      navDrive = "null";
+    }
+
+    if (gamePad0.getRawButton(5)){
+      navx.zeroYaw();
+      navx.reset();
+    }
+
+
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  public void turnThing(int angle, int time) {
+    setAngle = angle;
+    if(((int)challengeTimer.get()) == time) { //move forward to first ball
+      challengeTimer.stop();
+      driveTrain.tankDrive(0, 0);
+      if (Math.abs(angledYaw) <= 2) {
+        navDrive = "null";
+        challengeTimer.start();
+      } else {
+        navDrive = "Turn";
+        challengeTimer.stop();
+        }
+      }
+  }
+
+  public void curveDrive(double angle, double time){
+    if(curveTimer.get() == 0){
+      curveTimer.start();
+    }
+
+
+  }
+  public void recordInputs(double left, double right) {
+    File file = new File("inputs.txt");
+    if(file.exists()){
+      String leftStickString = "" + left;
+      String rightStickString = "" + right;
+      try {
+        FileWriter fileWriter = new FileWriter("inputs.txt");
+        fileWriter.write(leftStickString + "\n");
+        fileWriter.write(rightStickString + "\n");
+        fileWriter.flush();
+        fileWriter.close();
+      } catch(IOException e) {
+        System.out.println(e);
+      }
+    } else {
+      System.out.println("Error: No \"inputs.txt\" file in src/main/java/frc/robot");
+    }
+
+  }
+  public void readInputs() {
+    List<Integer> inputs = new ArrayList<>();
+  }
+
+}
